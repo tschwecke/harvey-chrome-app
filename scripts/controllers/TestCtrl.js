9926e39c3e5c9eb8dc1867f3cc49f82f0d1a45f4
Tim Schwecke <timothy.schwecke@pearson.com>
2013-06-17 08:05:43 -0600

    Allow adding and removing of setups and teardowns from tests

--- scripts/controllers/TestCtrl.js
+++ scripts/controllers/TestCtrl.js
@@ -1,235 +1,127 @@
- 
-function TestCtrl($scope, HarveyContext) {
-	var _colors = ['#E7FAFA', 'lightyellow', 'lightgreen', 'lightred'];
-	$scope.context = HarveyContext;
-	$scope.state = '';
+function TestCtrl($scope, HarveyContext, NavigationSvc, RequestSvc, ResponseSvc, RollupSvc) {
 
-	$scope.$watch('context.upcomingView', function(newValue, oldValue) {
-		if (newValue != 'Test')
-			$scope.state = 'fadingOut';
+	$scope.state = '';
+	$scope.test = HarveyContext.currentTest;
+	$scope.id = $scope.test.id;
+	$scope.changed = false;
+
+	configureTypeAheads(HarveyContext);
+	
+	NavigationSvc.setNavigateAwayCallback(function() {
+		$scope.state = 'fadingOut';
 	});
 
 	setTimeout(function() {
 		$scope.$apply(function() {
 			$scope.state = 'fadingIn';
-			console.log('now');
 		});
 	}, 1);
 
+	RequestSvc.currentRequest = RollupSvc.rollUpRequest($scope.test.request, HarveyContext.data.requestTemplates);
+	ResponseSvc.currentResponse = RollupSvc.rollUpResponse($scope.test.expectedResponse, HarveyContext.data.responseTemplates);
 
-
-	$scope.getColor = function(index) {
-		return _colors[index];
-	}
-
-	$scope.updateBgColor = function(field) {
-		if(field.value == field.inheritedValue) {
-			field.bgcolor = $scope.color[field.inheritedFrom] || transparent;
-		}
-		else {
-			field.bgcolor = 'transparent';
-		}
-	}
-
-	$scope.updateHeaderBgColor = function(field) {
-		if(field.key == field.inheritedKey && field.value == field.inheritedValue) {
-			field.bgcolor = $scope.color[field.inheritedFrom] || transparent;
-		}
-		else {
-			field.bgcolor = 'transparent';
-		}
-	}
-
-
-	$scope.addHeader = function() {
-		if(!$scope.currentTest.request.headers) $scope.currentTest.request.headers = [];
-
-		$scope.currentTest.request.headers.push({'key': '', 'value': ''});
-	}
-
-	$scope.removeHeader = function(index) {
-		$scope.currentTest.request.headers.splice(index, 1);
-	}
-
-	$scope.addQuerystring = function() {
-		if(!$scope.currentTest.request.querystring) $scope.currentTest.request.querystring = [];
-
-		$scope.currentTest.request.querystring.push({'key': '', 'value': ''});
-	}
-
-	$scope.removeQuerystring = function(index) {
-		delete $scope.currentTest.request.querystring.splice(index, 1);
-	}
-
-	$scope.updateModelWithBody = function(bodyText) {
-		$scope.currentTest.request.body = bodyText;
-	}
-
-
-	var rollupTest = function(test, testSuite) {
-		var rolledUpTest = $.extend(true, {}, test);
-
-		rolledUpTest.request.method = { "value": null, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
-		rolledUpTest.request.protocol = { "value": null, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
-		rolledUpTest.request.host = { "value": null, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
-		rolledUpTest.request.resource = { "value": null, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
-
-		if(test.request.templates) {
-			for(var i=0; i<test.request.templates.length; i++) {
-				var templateId = test.request.templates[i];
-				var template = null;
-				//Find the template
-				if(testSuite.requestTemplates) {
-					for(var j=0; j<testSuite.requestTemplates.length; j++) {
-						if(testSuite.requestTemplates[j].id === templateId) {
-							template = testSuite.requestTemplates[j];
-							break;
-						}
-					}
-				}
-
-				if(template != null) {
-					if(template.method) {
-						rolledUpTest.request.method.value = template.method;
-						rolledUpTest.request.method.inheritedValue = template.method;
-						rolledUpTest.request.method.inheritedFrom = template.id;
-						rolledUpTest.request.method.bgcolor = _colors[i];
-					}
-					if(template.protocol) {
-						rolledUpTest.request.protocol.value = template.protocol;
-						rolledUpTest.request.protocol.inheritedValue = template.protocol;
-						rolledUpTest.request.protocol.inheritedFrom = template.id;
-						rolledUpTest.request.protocol.bgcolor = _colors[i];
-					}
-					if(template.host) {
-						rolledUpTest.request.host.value = template.host;
-						rolledUpTest.request.host.inheritedValue = template.host;
-						rolledUpTest.request.host.inheritedFrom = template.id;
-						rolledUpTest.request.host.bgcolor = _colors[i];
-					}
-					if(template.resource) {
-						rolledUpTest.request.resource.value = template.resource;
-						rolledUpTest.request.resource.inheritedValue = template.resource;
-						rolledUpTest.request.resource.inheritedFrom = template.id;
-						rolledUpTest.request.resource.bgcolor = _colors[i];
-					}
-				}
-			}
+	var originalRequest = null;
+	$scope.request = RequestSvc.currentRequest;
+	$scope.$watch('request', function(newValue, oldValue) {
+		if(!originalRequest) {
+			originalRequest = JSON.stringify(newValue);
 		}
 
-		if(test.request.method) {
-			rolledUpTest.request.method = { "value": test.request.method, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
-		}
-		if(test.request.protocol) {
-			rolledUpTest.request.protocol = { "value": test.request.protocol, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
-		}
-		if(test.request.host) {
-			rolledUpTest.request.host = { "value": test.request.host, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
-		}
-		if(test.request.resource) {
-			rolledUpTest.request.resource = { "value": test.request.resource, "inheritedValue": null, "inheritedFrom": null, "bgcolor": "transparent" };
+		var currentRequest = JSON.stringify(newValue);
+		$scope.changed = (originalRequest !== currentRequest);
+	}, true);
+
+	var originalResponse = null;
+	$scope.response = ResponseSvc.currentResponse;
+	$scope.$watch('response', function(newValue, oldValue) {
+		if(!originalResponse) {
+			originalResponse = JSON.stringify(newValue);
 		}
 
-		return rolledUpTest;
-	}
+		var currentResponse = JSON.stringify(newValue);
+		$scope.changed = (originalResponse !== currentResponse);
+	}, true);
 
-	var doGetCaretPosition = function(ctrl) {
-		var CaretPos = 0;
-		// IE Support
-		if (document.selection) {
-			ctrl.focus ();
-			var Sel = document.selection.createRange ();
-			Sel.moveStart ('character', -ctrl.value.length);
-			CaretPos = Sel.text.length;
+	$scope.$watch('id', function(newValue, oldValue) {
+		$scope.changed = (newValue !== oldValue);
+	});
+
+
+
+	$scope.keep = function() {
+		$scope.test.id = $scope.id;
+		RequestSvc.populateTemplateWithRequest($scope.test.request);
+		ResponseSvc.populateTemplateWithResponse($scope.test.expectedResponse);
+
+		NavigationSvc.navigate('TestList');
+	};
+
+	$scope.discard = function() {
+		NavigationSvc.navigate('TestList');
+	};
+
+	//Need to do this workaround since angular doesn't support keypresses very well
+	$('#newSetupId').keypress(function(e) {
+		if(e.which == 13) {
+			$scope.$apply(function() {
+				$scope.addSetup();
+			});
 		}
-		// Firefox support
-		else if (ctrl.selectionStart || ctrl.selectionStart == '0')
-			CaretPos = ctrl.selectionStart;
-		return (CaretPos);
+	});
+
+	$scope.addSetup = function() {
+		if($scope.newSetupId) {
+			//Need to do this workaround for getting the value since angular and the bootstrap typeahead don't mix well
+			var newSetupId = $('#newSetupId').val();
+			$scope.test.setup.push(newSetupId);
+			$scope.newSetupId = '';
+		}
+
+		$scope.showAddSetup = false;
+	};
+
+	$scope.removeSetup = function(index) {
+		delete $scope.test.setup.splice(index, 1);
 	}
 
-	var beautify = function() {
-		editor.setValue(js_beautify(editor.getValue()));
+	//Need to do this workaround since angular doesn't support keypresses very well
+	$('#newTeardownId').keypress(function(e) {
+		if(e.which == 13) {
+			$scope.$apply(function() {
+				$scope.addTeardown();
+			});
+		}
+	});
+
+	$scope.addTeardown = function() {
+		if($scope.newTeardownId) {
+			//Need to do this workaround for getting the value since angular and the bootstrap typeahead don't mix well
+			var newSetupId = $('#newTeardownId').val();
+			$scope.test.teardown.push(newSetupId);
+			$scope.newSetupId = '';
+		}
+
+		$scope.showAddTeardown = false;
+	};
+
+	$scope.removeTeardown = function(index) {
+		delete $scope.test.teardown.splice(index, 1);
 	}
-
-	var editor;
-
-	var wireupForm = function($scope, bodyText, bodyCallback) {
-
-		if(_.isObject(bodyText)) bodyText = js_beautify(JSON.stringify(bodyText));
-
-		$('#resource').typeahead({
-			"source": ["courseId", "courseItemId", "userId", "gradeId"],
-			"matcher": function(item) {
-				var cursorPosition = doGetCaretPosition(document.getElementById('resource'));
-				var braceOpenPosition = this.query.lastIndexOf('${', cursorPosition);
-				var braceClosedPosition = this.query.lastIndexOf('}', cursorPosition);
-				if(cursorPosition>= 2 && braceOpenPosition > -1 && braceOpenPosition > braceClosedPosition) {
-					var partialVarName = this.query.substring(braceOpenPosition + 2, cursorPosition);
-
-					if(partialVarName === "" || item.indexOf(partialVarName) === 0) {
-						return true;
-					}
-				}
-
-				return false;
-			},
-			"updater": function(item) {
-				var cursorPosition = doGetCaretPosition(document.getElementById('resource'));
-				var braceOpenPosition = this.query.lastIndexOf('${', cursorPosition);
-				var braceClosedPosition = this.query.indexOf('}', cursorPosition);
-				var nextBraceOpenPosition = this.query.indexOf('${', cursorPosition);
-				if(nextBraceOpenPosition === -1) nextBraceOpenPosition = 99999999;
-				var endPosition = (braceClosedPosition !== -1 && braceClosedPosition < nextBraceOpenPosition) ? braceClosedPosition + 1 : cursorPosition;
-				var preString = this.query.substr(0, braceOpenPosition);
-				var postString = this.query.substr(endPosition);
-				var updatedText = preString + '${' + item + '}' + postString;
-				return updatedText;
-			}
-		});
-
-		editor = ace.edit("requestBody");
-		//editor.setReadOnly(true);
-		editor.renderer.setShowGutter(false);
-		editor.setHighlightActiveLine(false);
-		editor.setTheme("ace/theme/clouds");
-		editor.getSession().setMode("ace/mode/json");
-
-		editor.getSession().on('change', function(e) {
-			//Save the content
-			bodyCallback(editor.getValue());
-
-			//Set the editor height
-			var lineCount = editor.session.getLength();
-			if(lineCount < 5) lineCount = 5;
-			document.getElementById('requestBody').style.height = (lineCount * 15) + 'px';
-			editor.resize();
-
-			//Determine if the current content is valid
-			var text = editor.getValue();
-			$scope.validjson = true;
-			try {
-				JSON.parse(text);
-			}
-			catch(error) {
-				$scope.validjson = false;
-			}
-		});
-
-		editor.setValue(bodyText);
-
-	}
-
-	$scope.currentTest = rollupTest($scope.context.currentTest, $scope.context.data);
-	wireupForm($scope, $scope.currentTest.request.body, $scope.updateModelWithBody);
-
-	//Assign template colors
-	$scope.color = {};
-	for(var i=0; i<$scope.currentTest.request.templates.length; i++) {
-		$scope.color[$scope.currentTest.request.templates[i]] = _colors[i];
-	}
-
-
 }
 
+var configureTypeAheads = function(HarveyContext) {
+	var setupIds = [];
+	for(var i=0; i<HarveyContext.data.setupAndTeardowns.length; i++) {
+		setupIds.push(HarveyContext.data.setupAndTeardowns[i].id);
+	}
 
+	$('#newSetupId').typeahead({
+		"source": setupIds
+	});
+
+	$('#newTeardownId').typeahead({
+		"source": setupIds
+	});
+
+
+};
